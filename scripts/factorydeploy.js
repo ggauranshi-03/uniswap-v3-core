4b1e40d96d95b3ba9fd38222daa76c1b28d7a6b3
Gauranshi Gupta <95520163+ggauranshi-03@users.noreply.github.com>
2024-10-23 12:18:31 +0530

    extra scripts del

--- scripts/factorydeploy.js
+++ scripts/factorydeploy.js
@@ -28,31 +28,25 @@ const { ethers } = require('hardhat')
 const readlineSync = require('readline-sync')
 
 async function main() {
-  // Prompt user for DEX name and owner address
   const dexName = readlineSync.question('Enter DEX Name: ')
   const dexOwner = readlineSync.question('Enter DEX Owner Address: ')
 
-  // Fetch the contract to deploy
   const UniswapV3Factory = await ethers.getContractFactory('UniswapV3Factory')
 
-  // Deploy the contract
   console.log('Deploying UniswapV3Factory with the following parameters:')
   console.log(`DEX Name: ${dexName}`)
   console.log(`DEX Owner: ${dexOwner}`)
 
   const uniswapV3Factory = await UniswapV3Factory.deploy(dexName, dexOwner)
 
-  // Wait for the contract to be deployed
   await uniswapV3Factory.deployed()
 
   console.log('UniswapV3Factory deployed to:', uniswapV3Factory.address)
 
-  // Verify the contract on Etherscan
   console.log('Verifying contract on Etherscan...')
   await verifyContract(uniswapV3Factory.address, [dexName, dexOwner])
 }
 
-// Function to verify the contract on Etherscan
 async function verifyContract(address, constructorArguments) {
   try {
     await run('verify:verify', {
@@ -65,7 +59,6 @@ async function verifyContract(address, constructorArguments) {
   }
 }
 
-// Execute the deployment script
 main()
   .then(() => process.exit(0))
   .catch((error) => {
