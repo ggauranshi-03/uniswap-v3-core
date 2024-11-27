d13500b31a0dc8c90f1a3c4678b8bac468e388f7
Gauranshi Gupta <95520163+ggauranshi-03@users.noreply.github.com>
2024-11-27 17:43:52 +0530

    dependency

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,28 +1,4 @@
-// require('@nomiclabs/hardhat-waffle')
-// require('@nomiclabs/hardhat-ethers')
-// require('@nomiclabs/hardhat-etherscan')
 
-// module.exports = {
-//   defaultNetwork: 'sepolia',
-//   networks: {
-//     hardhat: {},
-//     sepolia: {
-//       url: 'https://sepolia.infura.io/v3/13322e29abd54fd19276fbdef62377f2',
-//       chainId: 11155111,
-//       accounts: ['cdff9d82ae22635d0f04601ee5bc0454944e9fb95d53c5817a8da3be9715358b'],
-//     },
-//   },
-
-//   solidity: {
-//     version: '0.7.6',
-//     settings: {
-//       optimizer: {
-//         enabled: true,
-//         runs: 200,
-//       },
-//     },
-//   },
-// }
 require('@nomiclabs/hardhat-waffle')
 require('@nomiclabs/hardhat-ethers')
 require('@nomiclabs/hardhat-etherscan')
